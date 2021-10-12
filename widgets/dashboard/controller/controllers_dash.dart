ddb768e27b78338f6919636256a5ec91fa034bbb
Marianny Fidelis <mariannyfidelis@gmail.com>
2021-10-12 16:18:48 -0300

    Lib bruno

--- widgets/dashboard/controller/controllers_dash.dart
+++
@@ -1,6 +0,0 @@
-import 'menu_controller_dash.dart';
-import 'navigation_controller_dash.dart';
-//import '/widgets/dashboard/controller/navigation_controller_dash.dart';
-
-MenuControllerDash menuControllerDash = MenuControllerDash.instance;
-NavigationControllerDash navigationController = NavigationControllerDash.instance;
